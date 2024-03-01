1d57b4fbd32a8195eafae21371280959d3652b97
mvitale2 <122138703+mvitale2@users.noreply.github.com>
2024-02-29 19:02:04 -0500

    did more dropdown work

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,31 @@
+function showFilterContent(id) {
+  document.getElementById(id).classList.toggle('show');
+  document.getElementById(`${id}-chev`).classList.toggle("chevron-rotate");
+}
+
+/* attempts to be smart and apply clickiness to all filters without having to do anything manually
 //retrieved from https://www.w3schools.com/howto/howto_js_dropdown.asp
-function showFilterContent() {
-    document.getElementById("year").classList.toggle("show");
-    document.getElementById("year-chev").classList.toggle("chevron-rotate")
+function showFilterContent(event) {
+  const targetElement = event.target;
+  targetElement.classList.toggle("show");
+  console.log(`Clicked element: ${event.target.textContent}`);
+  //document.getElementById("year-chev").classList.toggle("chevron-rotate")
+}
+
+filters = document.querySelectorAll("filter-header");
+
+filters.forEach((filter) => {
+  filter.addEventListener("click", showFilterContent);
+});
+
+function handleFilterClick(event) {
+  const targetElement = event.target;
+  targetElement.classList.toggle('show');
+  console.log(`Clicked element: ${event.target}`)
 }
 
 // closes the dropdown if the user clicks the dropdown or another part of the page while shown
-/*
+
 window.onclick = function (event) {
   if (!event.target.matches(".filter-header")) {
     var dropdowns = document.getElementsByClassName("filter-content");
@@ -19,3 +39,12 @@ window.onclick = function (event) {
   }
 };
 */
+
+//control variables
+let isSearching = false;
+let page = 1;
+
+function createCarCard (car) {
+  const {year, make, model, mileage, price, color, gasMileage} = car;
+}
+
