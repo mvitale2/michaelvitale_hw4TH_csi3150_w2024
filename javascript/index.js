5e66ac7dbb61b19bb82598d2ad571c0af194cc53
mvitale2 <122138703+mvitale2@users.noreply.github.com>
2024-03-03 13:48:03 -0500

    made progress on filter logic

--- javascript/index.js
+++ javascript/index.js
@@ -28,8 +28,33 @@ function createCarCard(car) {
   `;
 }
 
+function createFilteredCarCard(car, filters) {
+  const { year, color, make, model, price, mileage, gasMileage } = car;
+  for (let i = 0; i < filters.length; i++) {
+    if (Object.keys(car).includes(filters[i])) {
+      return `
+      <div class="car-card">
+        <img src="./assets/images/${make}-${model}.png" alt="${color}-${make}-${model}" />
+        <div class="car-card-info">
+          <h2>${make} ${model}</h2>
+          <p>${year}</p>
+          <p>$${price}</p>
+          <p>Mileage: ${mileage}</p>
+          <p>Gas Mileage: ${gasMileage}</p>
+          <p>Color: ${color}</p>
+        </div>
+      </div>
+      `;
+    }
+  }
+}
+
 function generateCarCards(cars) {
   return cars.map((car) => createCarCard(car)).join("");
+}
+
+function generateFilteredCarCards(cars, filters) {
+  return cars.map((car) => createFilteredCarCard(car, filters)).join("");
 }
 
 // color filter
@@ -44,7 +69,7 @@ function getCarColors(cars) {
 function createColorFilter(color) {
   return `
     <div>
-      <input type="checkbox" name="color" id="${color} value=${color}" />
+      <input type="checkbox" name="color" id="${color}" value="${color}" />
       <label for="${color}">${color}</label>
     </div>
     `;
@@ -78,7 +103,45 @@ function generateMakeFilters(makes) {
 
 //filter logic
 
+const colors = getCarColors(usedCars);
+const makes = getCarMakes(usedCars);
+const mileages = ["15", "30", "45", "60"];
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var checkedCheckboxes = document.querySelectorAll(
+    "input[type=checkbox]:checked"
+  );
+  var choiceFilters = [];
+  checkedCheckboxes.forEach(function (checkbox) {
+    if (colors.includes(checkbox.id)) {
+      choiceFilters.push(checkbox.id);
+      console.log(choiceFilters);
+    } else if (makes.includes(checkbox.id)) {
+      choiceFilters.push(checkbox.id);
+      console.log(choiceFilters);
+    } else if (mileages.includes(checkbox.id)) {
+      choiceFilters.push(checkbox.id);
+      console.log(choiceFilters);
+    }
+  });
+  console.log(choiceFilters);
+  console.log(generateFilteredCarCards(usedCars, choiceFilters));
+  container.innerHTML = generateFilteredCarCards(usedCars, choiceFilters);
+});
+
+/*
+function getCheckedColors () {
+  const selectedColors = new Set();
+  colorFilters.forEach(colorFilter => {
+    if (colorFilter.getElementById(`${colorFilter}`).checked == true) {
+      selectedColors.add(color);
+    }
+  });
+  console.log(selectedColors);
+  return [...selectedColors];
+}
+*/
 
 //initialize on page load
 
