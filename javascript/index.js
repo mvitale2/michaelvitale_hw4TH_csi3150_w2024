84f9587577faaf9446a9e33362fa4d4c0bb3021e
Skel <mvitale738@gmail.com>
2024-03-07 22:16:09 -0500

    completed filter functionality. added responsive styling.

--- javascript/index.js
+++ javascript/index.js
@@ -7,8 +7,10 @@ const container = document.getElementById("content-container");
 const color = document.getElementById("color");
 const make = document.getElementById("make");
 const year = document.getElementById("year");
+/*
 const price = document.getElementById("price");
 const mileage = document.getElementById("mileage");
+*/
 
 function clearContainer() {
   container.innerHTML = "";
@@ -19,7 +21,9 @@ function createCarCard(car) {
   const { year, color, make, model, price, mileage, gasMileage } = car;
   return `
   <div class="car-card">
-    <img src="./assets/images/${make}-${model}.png" alt="${color}-${make}-${model}" />
+    <div class="card-img-container">
+      <img src="./assets/images/${make}-${model}.png" alt="${color}-${make}-${model}" />
+    </div>
     <div class="car-card-info">
       <h2>${make} ${model}</h2>
       <p>${year}</p>
@@ -33,6 +37,7 @@ function createCarCard(car) {
 }
 
 function generateCarCards(cars) {
+  clearContainer();
   return cars.map((car) => createCarCard(car)).join("");
 }
 
@@ -49,7 +54,7 @@ function getCarColors(cars) {
 function createColorFilter(color) {
   return `
     <div>
-      <input type="checkbox" name="color" id="${color}" value="${color}" />
+      <input type="radio" name="color" id="${color}" value="${color}" />
       <label for="${color}">${color}</label>
     </div>
     `;
@@ -75,7 +80,7 @@ function getCarYears(cars) {
 function createYearFilter(year) {
   return `
   <div>
-    <input type="checkbox" name="year" id="${year}" />
+    <input type="radio" name="year" id="${year}" />
     <label for="${year}">${year}</label>
   </div>
   `;
@@ -98,7 +103,7 @@ function getCarMakes(cars) {
 function createMakeFilter(make) {
   return `
   <div>
-    <input type="checkbox" name="make" id="${make}" />
+    <input type="radio" name="make" id="${make}" />
     <label for="${make}">${make}</label>
   </div>
   `;
@@ -113,20 +118,25 @@ function generateMakeFilters(makes) {
 const colors = getCarColors(usedCars);
 const makes = getCarMakes(usedCars);
 const years = getCarYears(usedCars).toString(); // since year is int in usedCars.js
+const prices = ["16000", "20000", "40000", "50000"];
+const mileages = ["15000", "30000", "45000", "60000"];
 
 //apply filters button
-
+//filter logic is a lot harder than I would have ever thought ;)
+//or maybe i'm only making it harder...
 form.addEventListener("submit", (event, car) => {
   event.preventDefault();
 
   var checkedCheckboxes = document.querySelectorAll(
-    "input[type=checkbox]:checked"
+    "input[type=radio]:checked"
   );
   console.log(checkedCheckboxes);
   var choiceFilters = [];
+  var priceFilters = [];
+  var mileageFilters = [];
   if (checkedCheckboxes.length === 0) {
+    var filteredCars = [""];
     console.log("No filters selected, resetting page...");
-    clearContainer();
     container.innerHTML = generateCarCards(usedCars);
   } else {
     checkedCheckboxes.forEach((checkbox) => {
@@ -136,20 +146,29 @@ form.addEventListener("submit", (event, car) => {
         choiceFilters.push(checkbox.id);
       } else if (years.includes(checkbox.id)) {
         choiceFilters.push(checkbox.id);
+      } else if (prices.includes(checkbox.id)) {
+        priceFilters.push(checkbox.id);
+      } else if (mileages.includes(checkbox.id)) {
+        mileageFilters.push(checkbox.id);
       }
       console.log(choiceFilters);
       //filter documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-      const filteredCars = usedCars.filter((car) => {
+      filteredCars = usedCars.filter((car) => {
         return (
-          choiceFilters.includes(car.color) ||
-          choiceFilters.includes(car.year.toString()) ||
-          choiceFilters.includes(car.make)
+          choiceFilters.every(
+            (filter) =>
+              car.color === filter ||
+              car.year === parseInt(filter) ||
+              car.make === filter
+          ) &&
+          priceFilters.every( filter => car.price <= filter) &&
+          mileageFilters.every( filter => car.mileage >= filter)
         );
-        //Need to make this exclusive. Currently it includes every car that matches just one of these filters.
-        //Also need to implement functionality for mileage & price.
       });
-      clearContainer();
-      container.innerHTML = generateCarCards(filteredCars);
+      container.innerHTML =
+        filteredCars.length > 0
+          ? generateCarCards(filteredCars)
+          : "<p>Sorry, we don't possess any cars that match your filters. :(</p>";
     });
   }
 });
@@ -157,10 +176,11 @@ form.addEventListener("submit", (event, car) => {
 //clear button
 
 document.getElementById("clear-btn").addEventListener("click", function () {
-  var checkedCheckboxes = document.querySelectorAll("input[type=checkbox]");
+  var checkedCheckboxes = document.querySelectorAll("input[type=radio]");
   checkedCheckboxes.forEach((checkbox) => {
     checkbox.checked = false; // this blog post saved me https://aileenrae.co.uk/blog/programatically-check-uncheck-checkbox-javascript/
   });
+  container.innerHTML = generateCarCards(usedCars);
 });
 
 //initialize on page load
